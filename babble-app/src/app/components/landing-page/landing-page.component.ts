ce90fbb50900c6853dab662b9cbd541483ab1df6
kkabra <kkabra@users.github.com>
2020-07-20 11:40:01 +0530

    Contact Enqiry PopUp

--- babble-app/src/app/components/landing-page/landing-page.component.ts
+++ babble-app/src/app/components/landing-page/landing-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, TemplateRef } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { TeamBabbleApp } from "./babbleapp-team";
 import { Router } from '@angular/router';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 declare var $: any;
 @Component({
   selector: 'app-landing-page',
@@ -14,7 +15,7 @@ export class LandingPageComponent implements OnInit, OnChanges {
 
   //for mobile device
   isMenuOpen:boolean = false;
-
+  bsModalRef: BsModalRef;
   contactForm: FormGroup;
 
   displayElememnt = {
@@ -25,7 +26,7 @@ export class LandingPageComponent implements OnInit, OnChanges {
   teamBabbleApp = new TeamBabbleApp();
 
   constructor(public builder: FormBuilder,
-    public router: Router, private db: AngularFireDatabase){
+    public router: Router, private db: AngularFireDatabase, private bsModalService: BsModalService){
 
     this.contactForm = this.builder.group({
       name: new FormControl(null,Validators.required),
@@ -62,7 +63,7 @@ export class LandingPageComponent implements OnInit, OnChanges {
     this.router.navigate(['tutor-enquiry']);
   }
 
-  submitEnquiry(){
+  submitEnquiry(template: TemplateRef<any>){
     if (this.contactForm.controls.name.value != "" &&
       this.contactForm.controls.contact.value != "" &&
       this.contactForm.controls.message.value != "") {
@@ -73,9 +74,17 @@ export class LandingPageComponent implements OnInit, OnChanges {
       })
       this.contactForm.reset();
       window.scrollTo(0, 0);
+      this.openModal(template);
     } else{
       console.log("Data Missing!");
     }
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.bsModalRef = this.bsModalService.show(template, { class: 'modal-dialog-centered' });
+  }
+  closeModal() {
+    this.bsModalRef.hide();
   }
 
 }
